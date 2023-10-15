582165fd737f6a4aa05ba1fa93fda1258a273bdf
Yuvraj <yuvrajm@iitbhilai.ac.in>
2023-10-15 16:46:42 +0530

    linking done , need saurav's loading screen

--- Semester 1/buttonAction.js
+++ Semester 1/buttonAction.js
@@ -2,39 +2,31 @@ document.getElementById("redirectButton").addEventListener("click", function() {
     var selectedCourse = document.getElementById("semester").value;
     var selectedOption = document.getElementById("Branch").value;
 
-    if (selectedCourse === "ic100") {
-        if (selectedOption === "notes") {
-            window.location.href = "ic100_notes.html";
-        } else {
-            window.location.href = "ic100_tut.html";
+    var courseUrls = {
+        "ic100": {
+            "notes": "IC100/ic100.html",
+            "other": "IC100/ic100tut.html"
+        },
+        "ic101": {
+            "notes": "ic101_notes.html",
+            "other": "ic101_tut.html"
+        },
+        "ic103": {
+            "notes": "ic103_notes.html",
+            "other": "ic103_tut.html"
+        },
+        "ic104": {
+            "notes": "ic104_notes.html",
+            "other": "ic104_tut.html"
+        },
+        "ic105": {
+            "notes": "ic105_notes.html",
+            "other": "ic105_tut.html"
         }
-    }
-    if (selectedCourse === "ic101") {
-        if (selectedOption === "notes") {
-            window.location.href = "ic101_notes.html";
-        } else {
-            window.location.href = "ic101_tut.html";
-        }
-    }
-    if (selectedCourse === "ic103") {
-        if (selectedOption === "notes") {
-            window.location.href = "ic103_notes.html";
-        } else {
-            window.location.href = "ic103_tut.html";
-        }
-    }
-    if (selectedCourse === "ic104") {
-        if (selectedOption === "notes") {
-            window.location.href = "ic104_notes.html";
-        } else {
-            window.location.href = "ic104_tut.html";
-        }
-    }
-    if (selectedCourse === "ic105") {
-        if (selectedOption === "notes") {
-            window.location.href = "ic105_notes.html";
-        } else {
-            window.location.href = "ic105_tut.html";
-        }
+    };
+
+    if (courseUrls[selectedCourse]) {
+        var url = courseUrls[selectedCourse][selectedOption] || courseUrls[selectedCourse]["other"];
+        window.location.href = url;
     }
 });
